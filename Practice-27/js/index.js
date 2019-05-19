ddc66ed734e56607dca30aade5ba80d320bf40f2
majidazarasa <46866429+majidazarasa@users.noreply.github.com>
2019-05-19 18:03:43 +0430

    Add files via upload

---
+++ Practice-27/js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mainPage").style.marginRight = "250px"; 
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mainPage").style.marginRight = "0px"; 
+}
